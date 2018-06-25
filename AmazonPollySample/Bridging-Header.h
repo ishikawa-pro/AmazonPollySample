01e2880495fc65aeb1a5fc828376a745d34bce15
ishikawa-pro <ishikawa.pro@gmail.com>
2018-06-25 21:49:53 +0900

    initial commit

---
+++ AmazonPollySample/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  AmazonPollySample
+//
+//  Created by Ishikawa Akira on 2018/06/25.
+//  Copyright © 2018年 Ishikawa Akira. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <AWSCore/AWSCore.h>
+#import <AWSCognito/AWSCognito.h>
+
+#endif /* Bridging_Header_h */
---
+++ AmazonPollySample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmazonPollySample
+//
+//  Created by Ishikawa Akira on 2018/06/25.
+//  Copyright © 2018年 Ishikawa Akira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
